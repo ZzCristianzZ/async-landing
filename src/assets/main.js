9562cf1c96a19e8ea1f2bf0d0f931f3033062fcb
ZzCristianzZ <crisfegomez@gmail.com>
2023-02-09 22:45:04 -0500

    Se termino de corregir algunas lineas de codigo, asi mismo se vinculo el archivo main.js con nuestro index.html, para lograr el objetivo de consumir la API y por ultimo se agrego a la funcion Autoejecutable sus parentesis para ser llamada

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,14 +1,10 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC9k0tTsh_qStx0HPuPftSsg&part=snippet%2Cid&order=date&maxResults=5';
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1d978f73ffmshe9133ac38b73533p114a1fjsnd598fa6d14c6',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-	}
-};
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
 
 const content = null || document.getElementById('content');
+
+const options={method:'GET',headers:{
+    'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com','X-RapidAPI-Key':'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
+}};
 
 // La logica de async : ir por los datos, esperar por ellos y finalmente retornarlos para transformalos a Objeto JSON
 async function fetchData(urlApi){
@@ -28,7 +24,7 @@ async function fetchData(urlApi){
         <div class="group relative">
             <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
@@ -39,7 +35,8 @@ async function fetchData(urlApi){
         </div>` ).slice(0,4).join('')}
            
         `;
-    } catch {
-
+        content.innerHTML = view;
+    } catch (error){
+        console.log(error);
     }
-})
+})();
